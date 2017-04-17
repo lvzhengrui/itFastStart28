0302fa61c76c351968c11c6eed7036093ed1e809
lvzhengrui <38126506@qq.com>
2017-04-17 14:35:11 +0800

    首次提交

--- src/main/java/org/lvzr/fast/java/patten/create/abstractFactory/Test.java
+++ src/main/java/org/lvzr/fast/java/patten/create/abstractFactory/Test.java
@@ -12,6 +12,9 @@ import org.lvzr.fast.java.patten.create.factoryMethod.Sender;
  * 其实这个模式的好处就是，如果你现在想增加一个功能：发及时信息，则只需做一个实现类，
  * 实现Sender接口，同时做一个工厂类，实现Provider接口，就OK了，
  * 无需去改动现成的代码。这样做，拓展性较好！
+ * 
+ * 
+ * 所谓抽象工厂模式，即对工厂类进行抽象，所以根据不同具体产品类，就会对应一个具体工厂类
  */
 public class Test {  
   
