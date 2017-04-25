461098b3b393ab832aade0848dd43354319d6c68
lvzhengrui <38126506@qq.com>
2017-04-25 10:35:37 +0800

    首次提交

--- src/main/java/org/lvzr/fast/bigdata/solr/SolrTest.java
+++ src/main/java/org/lvzr/fast/bigdata/solr/SolrTest.java
@@ -44,8 +44,8 @@ public class SolrTest {
         //构造一篇文档  
          SolrInputDocument document = new SolrInputDocument();  
          //往doc中添加字段,在客户端这边添加的字段必须在服务端中有过定义  
-         document.addField("id", "8");  
-         document.addField("name", "周新星");  
+         document.addField("id", "5");  
+         document.addField("name", "周星驰");  
          document.addField("description", "一个灰常牛逼的军事家");  
          //获得一个solr服务端的请求，去提交  ,选择具体的某一个solr core
         HttpSolrClient solr = new HttpSolrClient(SOLR_URL + "abc");
@@ -75,20 +75,21 @@ public class SolrTest {
      * @throws Exception 
       */
      public void querySolr() throws Exception{
-         HttpSolrClient solrServer = new HttpSolrClient(SOLR_URL+"abc/");  
+         HttpSolrClient solrServer = new HttpSolrClient(SOLR_URL+"abc");  
          SolrQuery query = new SolrQuery();  
          //下面设置solr查询参数
          //query.set("q", "*:*");// 参数q  查询所有   
-         query.set("q","周星驰");//相关查询，比如某条数据某个字段含有周、星、驰三个字  将会查询出来 ，这个作用适用于联想查询
+         //query.set("q","name:*周*");//相关查询，比如某条数据某个字段含有周、星、驰三个字  将会查询出来 ，这个作用适用于联想查询
 
+         //参数df,给query设置默认搜索域
+         query.set("df", "name");  
+         query.set("q", "*周*");
+         
          //参数fq, 给query增加过滤查询条件  
-         query.addFilterQuery("id:[0 TO 9]");//id为0-4  
+         query.addFilterQuery("id:[0 TO 7]");//id为0-4  
 
          //给query增加布尔过滤条件  
          //query.addFilterQuery("description:演员");  //description字段中含有“演员”两字的数据
-
-         //参数df,给query设置默认搜索域  
-         query.set("df", "name");  
 
          //参数sort,设置返回结果的排序规则  
          query.setSort("id",SolrQuery.ORDER.desc);
@@ -101,6 +102,7 @@ public class SolrTest {
          query.setHighlight(true);  
          //设置高亮的字段  
          query.addHighlightField("name");  
+         
          //设置高亮的样式  
          query.setHighlightSimplePre("<font color='red'>");  
          query.setHighlightSimplePost("</font>"); 
@@ -132,7 +134,7 @@ public class SolrTest {
          SolrTest solr = new SolrTest();
          //solr.createSolrServer();
          solr.addDoc();
-         solr.deleteDocumentById();
          solr.querySolr();
+         //solr.deleteDocumentById();
     }
 }
