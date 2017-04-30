3395a57de47e1b42b6a7a7ecd251ffcc84a79a3b
lvzhengrui <38126506@qq.com>
2017-04-30 16:24:43 +0800

    首次提交

--- src/main/java/org/lvzr/fast/bigdata/zookeeper/README.md
+++ src/main/java/org/lvzr/fast/bigdata/zookeeper/README.md
@@ -1,3 +1,13 @@
 源自Google的Chubby论文，发表于2006年11月，Zookeeper是Chubby克隆版
 解决分布式环境下的数据管理问题：统一命名，状态同步，集群管理，配置同步等。
 
+
+启动
+bin/zkServer.sh start /conf/zoo_sample.cfg
+
+
+###zookeeper 入门讲解实例 转
+http://blackproof.iteye.com/blog/2039040
+
+
+
---
+++ src/main/java/org/lvzr/fast/bigdata/zookeeper/ZookeeperTest.java
@@ -0,0 +1,45 @@
+package org.lvzr.fast.bigdata.zookeeper;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+
+public class ZookeeperTest {
+
+	public static void main(String[] args) throws Exception {
+
+		// 创建一个Zookeeper实例，第一个参数为目标服务器地址和端口，第二个参数为Session超时时间，
+		//  第三个为节点变化时的回调方法
+		ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 500000, new Watcher() {
+			// 监控所有被触发的事件
+			public void process(WatchedEvent event) {
+				System.out.println("事件---"+event.getType());
+			}
+		});
+		
+		// 创建一个节点root，数据是mydata,不进行ACL权限控制，节点为永久性的(即客户端shutdown了也不会消失)
+		zk.create("/root", "mydata".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+		// 在root下面创建一个childone znode,数据为childone,不进行ACL权限控制，节点为永久性的
+		zk.create("/root/childone", "childone".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+		// 取得/root节点下的子节点名称,返回List<String>
+		zk.getChildren("/root", true);
+
+		// 取得/root/childone节点下的数据,返回byte[]
+		zk.getData("/root/childone", true, null);
+
+		// 修改节点/root/childone下的数据，第三个参数为版本，如果是-1，那会无视被修改的数据版本，直接改掉
+		zk.setData("/root/childone", "childonemodify".getBytes(), -1);
+
+		// 删除/root/childone这个节点，第二个参数为版本，－1的话直接删除，无视版本
+		zk.delete("/root/childone", -1);
+
+		// 关闭session
+		zk.close();
+
+	}
+
+}
