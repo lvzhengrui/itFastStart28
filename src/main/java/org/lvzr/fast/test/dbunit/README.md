ef91c8f4c5a3cc86e237f042be132280e98d1152
lvzhengrui <38126506@qq.com>
2017-05-03 16:17:58 +0800

    首次提交

---
+++ src/main/java/org/lvzr/fast/test/dbunit/README.md
@@ -0,0 +1,8 @@
+
+
+###DbUnit入门实战
+http://yangzb.iteye.com/blog/947292
+
+### 使用DBUnit做单元测试
+http://blog.csdn.net/fenglibing/article/details/16844473
+
---
+++ src/main/java/org/lvzr/fast/test/dbunit/TestDbunit.java
@@ -0,0 +1,19 @@
+package org.lvzr.fast.test.dbunit;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDbunit{
+	
+
+	@Before
+	public void init(){
+		
+	}
+	
+	@Test
+	public void testDataSet(){
+		
+	}
+	
+}
