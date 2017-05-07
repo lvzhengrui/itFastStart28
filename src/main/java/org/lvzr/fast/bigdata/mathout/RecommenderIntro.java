4cef7946bc526c87c1fdbea0e061afdeb98869cf
lvzhengrui <38126506@qq.com>
2017-05-07 13:14:57 +0800

    f

--- src/main/java/org/lvzr/fast/bigdata/mathout/RecommenderIntro.java
+++ src/main/java/org/lvzr/fast/bigdata/mathout/RecommenderIntro.java
@@ -1,15 +1,17 @@
 package org.lvzr.fast.bigdata.mathout;
 
-import org.apache.mahout.cf.taste.impl.model.file.*;  
-import org.apache.mahout.cf.taste.impl.neighborhood.*;  
-import org.apache.mahout.cf.taste.impl.recommender.*;  
-import org.apache.mahout.cf.taste.impl.similarity.*;  
-import org.apache.mahout.cf.taste.model.*;  
-import org.apache.mahout.cf.taste.neighborhood.*;  
-import org.apache.mahout.cf.taste.recommender.*;  
-import org.apache.mahout.cf.taste.similarity.*;  
-import java.io.*;  
-import java.util.*;  
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;  
 
 /**
  * 
